de5b4b01cc98163ddc5e22ada1e692812652d325
Ravi Kumar <68500915+raviroygit@users.noreply.github.com>
2021-01-24 13:58:50 +0530

    Update footer.component.jsx

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,7 @@ import "./footer.style.css"
 const FooterPanel = () => {
     return (
         <div>
-            <div className="py-2 text-center footer-style"> Er. Ravi Roy @ &nbsp; {new Date().getFullYear()}</div>
+            <div className="py-2 text-center footer-style">Develop by Er.Ravi Roy @ &nbsp; {new Date().getFullYear()}</div>
         </div>
     );
 };
